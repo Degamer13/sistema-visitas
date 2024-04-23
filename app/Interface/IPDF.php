241189b506a7cf9631799e3c2f68e30fd3fab050
anderson <andersontrabajos2@gmail.com>
2024-04-23 11:41:40 -0500

    fix: :bug: Arreglando el bug de muestra de pdf

--- app/Interface/IPDF.php
+++ app/Interface/IPDF.php
@@ -1,5 +1,5 @@
 <?php
 namespace App\Interface;
 interface IPDF {
-    public function TablaGenerica(array $objetos, string $name) : void;
+    public function TablaGenerica(array $objetos, string $name, bool $time) : void;
 }
